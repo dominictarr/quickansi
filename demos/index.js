c89c49d8a2672d12ca408945f5e76f86fffdec1e
Dominic Tarr <dominic.tarr@gmail.com>
2013-12-11 10:00:11 +0800

    support --fast option

--- demos/index.js
+++ demos/index.js
@@ -1,15 +1,19 @@
-var opts = require('optimist').argv
-var slow = require('modem-stream')(opts.baud || 300)
-slow.pipe(process.stdout)
+var opts = require('optimist').argv, stream
+if(opts.fast)
+  stream = process.stdout
+else {
+  stream = require('modem-stream')(opts.baud || 300)
+  stream.pipe(process.stdout)
+}
 
 require('ansi-recover')({cursor: false})
 
 module.exports = require('../') (
-  slow,
+  stream,
     opts.diff  ? require('../diff')
   : opts.multi ? require('../multiline')
   :              null
 )
 
 if(!module.parent)
-  console.log('USAGE: node demos/time.js 2> logfile')
+  console.log('USAGE: node demos/marquee.js --baud $MODEM_SPEED')
