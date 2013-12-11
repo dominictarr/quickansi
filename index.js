4320885d35847966bd300d528a9c9d0e8b4b5697
Dominic Tarr <dominic.tarr@gmail.com>
2013-12-11 10:23:43 +0800

    there isn't much code, so put it in one file

--- index.js
+++ index.js
@@ -1,73 +1,88 @@
-//generate patches to strings, which can be used to update ansi terminals!
-var multi = require('./multiline')
+var updateableDiff = require('./updateable')
 
-var stderr = process.stderr.isTTY ? function () {} : function (value) {
-  var inspect = require('util').inspect
-  console.error(inspect(value, {depth: 10}))
+function toLines (a) {
+  return a.split('\n')
 }
 
-function diff (a, b) {
- var changes = []
-  //iterate over a, b
-  var M = Math.max(a.length, b.length)
-  for(var i = 0; i < M; i++) {
-    if(a[i] && !b[i])
-      changes.push([+i, ' '])
-    else if(a[i] != b[i])
-      changes.push([+i, b[i]])
-  }
-    
-  return changes
+function toChars (a) {
+  return a.split('')
 }
 
-function apply (patch) {
-  var p = 0
-  return patch.map(function (e) {
-    var n = e[0]
-    var c = e[1]
-    return n === ++p ? c : move((p=n) + 1) + c
-  }).join('')
-}
-
-function move(x, current) {
+function move (x, current) {
   x = x || 0
   return '\x1b['+ x +'G'
 }
 
 function del (i, current) {
-//  return ''
-
-  var s = ''
-  while(i--)
-    s += '\x1b[3~'
-  return s
+  return '\x1b['+ i +'P'
 }
 
-function quick (stdout, opts) {
+function insert (n) {
+  return '\x1b['+ n +'@'
+}
+
+function moveLine(y) {
+  y = y || 0
+  return '\x1b['+ y +';0f'
+}
+
+function delLines (i) {
+  return '\x1b['+ i +'M'
+}
+
+function insertLines (n) {
+  return '\x1b['+ n +'L'
+}
+
+function applyChars (patch) {
+  var next = 0
+  return patch.map(function (op) {
+    return (op.at === next++ ? '' : move(next = op.at + 1)) +
+      ( op.type === 'update' ? op.value
+      : op.type === 'delete' ? del(op.value)
+      :                        insert(op.value.length) + op.value.join(''))
+  }).join('')
+}
+
+function applyLines (patch) {
+  var next = 0
+  return patch.map(function (op) {
+    return moveLine (op.at + 1) +
+      ( op.type === 'update' ? applyChars(op.value)
+      : op.type === 'delete' ? delLines(op.value)
+      :                        insertLines(op.value.length) + op.value.join('\n'))
+  }).join('')
+}
+
+var apply = applyLines
+
+function diff (a, b) {
+  var _a = toLines(a)
+  var _b = toLines(b)
+
+  return updateableDiff(_a, _b).map(function (e) {
+    if(e.type !== 'update') return e
+    return {
+      type: 'update', at: e.at,
+      value: updateableDiff(toChars(e.oldValue), toChars(e.value))
+    }
+  })
+}
+
+function quick (stdout) {
   stdout = stdout || process.stdout
-  stdout.write(move())
-  opts = opts || multi
   var current = ''
   return function update (a) {
-//    stdout.write(move(0, 0))
     if(!current)
       stdout.write(current = a)
     else {
-      var patch = opts.diff(current, current = a) //diff(current.split(''), (current = a).split(''))
-      stderr(patch)
-      stdout.write(opts.apply(patch))
+      var patch = diff(current, current = a)
+      stdout.write(apply(patch))
     }
   }
 }
 
-/*
-#okay, cool trick
-> nano | tee out
-#then, in another terminal
-> tail -f | cat -v
-# this shows the raw output of nano.
-# if you left off cat -v then it would
-# show a duplicate of nano!
-*/
-module.exports = quick
+exports = module.exports = quick
+exports.diff = diff
+exports.apply = applyLines
 
